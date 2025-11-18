9aa2f7679c27adbdcc7c13116c23071bd26d28a9
TawfiqAhmedRafi <rafi70722@gmail.com>
2025-11-18 23:53:37 +0600

    Initialized

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router";
+import RooyLayout from "../Layouts/RooyLayout";
+import Home from "../Pages/Home/Home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RooyLayout,
+    children: [
+      {
+        index: true,
+        Component: Home , 
+      },
+    ],
+  },
+]);
