526cdc2dc3ffb27e7dd098723f3c27f9e077f323
TawfiqAhmedRafi <rafi70722@gmail.com>
2025-11-24 07:06:49 +0600

    JWT token verification

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,11 +1,47 @@
-import axios from 'axios';
-import React from 'react';
+import axios from "axios";
+import React, { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router";
 
 const axiosSecure = axios.create({
-    baseURL : 'http://localhost:3000'
-})
+  baseURL: "http://localhost:3000",
+});
 const useAxiosSecure = () => {
-    return axiosSecure ;
+  const { user, logOut } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // intercept request
+    const requestInterceptor = axiosSecure.interceptors.request.use(
+      (config) => {
+        config.headers.Authorization = `Bearer ${user?.accessToken}`;
+        return config;
+      }
+    );
+    // interceptor response
+    const resInterceptors = axiosSecure.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        console.log(error);
+        const statusCode = error.status;
+        if (statusCode === 401 || statusCode === 403) {
+          logOut().then(() => {
+            navigate("/login");
+          });
+        }
+        return Promise.reject(error);
+      }
+    );
+
+    return () => {
+      axiosSecure.interceptors.request.eject(requestInterceptor);
+      axiosSecure.interceptors.response.eject(resInterceptors);
+    };
+  }, [user,logOut,navigate]);
+
+  return axiosSecure;
 };
 
 export default useAxiosSecure;
